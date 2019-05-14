bda6c3611aaee53c4f3d1f73931baeda99d42d79
13714093081@163.com <13714093081@163.com>
2019-05-14 10:07:13 +0800

    add build jar and build war package running on tomcat

--- microcloud-customer-80/src/main/java/com/microcloud/Application.java
+++ microcloud-customer-80/src/main/java/com/microcloud/Application.java
@@ -2,6 +2,8 @@ package com.microcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,9 +13,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.microcloud.feignservice") // 扫描 feign 接口定义的包
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class); // 打成 war 包后执行
     }
 }
--- microcloud-dashboard-hystrix-9001/src/main/java/com/microcloud/HystrixDashboardStartMain.java
+++ microcloud-dashboard-hystrix-9001/src/main/java/com/microcloud/HystrixDashboardStartMain.java
@@ -2,6 +2,8 @@ package com.microcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
@@ -11,9 +13,14 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringBootApplication
 @EnableHystrixDashboard
-public class HystrixDashboardStartMain {
+public class HystrixDashboardStartMain extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardStartMain.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HystrixDashboardStartMain.class); // 打成 war 包后执行
     }
 }
