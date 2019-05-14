f17511519be8202c68c1b95566b50d9c06940fe3
13714093081@163.com <13714093081@163.com>
2019-05-14 20:57:24 +0800

    add spring cloud gateway

---
+++ microcloud-gateway-9501/src/main/java/com/microcloud/GateWayMainStart.java
@@ -0,0 +1,16 @@
+package com.microcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Auther: wangjian
+ */
+@SpringBootApplication
+public class GateWayMainStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMainStart.class, args);
+    }
+}
