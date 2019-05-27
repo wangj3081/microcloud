46896ad8711f75fb8b3e1b9adfd8c10c2d31a267
王剑 <wangjian01@coreware.cn>
2019-05-28 00:12:00 +0800

    update gateway ribbon merge Bug

--- microcloud-customer-80/src/main/java/com/microcloud/config/MyLoadBalancedConfig.java
+++ microcloud-customer-80/src/main/java/com/microcloud/config/MyLoadBalancedConfig.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @Auther: wangjian
  */
-@Configuration
+//@Configuration // 自定义的 ribbon 负载均衡策略在与 gateway 的整合中总出现异常，目前未深究，采取默认的负载处理
 public class MyLoadBalancedConfig {
 
     // 自定义负载均衡策略
-    @Bean
+//    @Bean
     public IRule ribbonRule() {
      return  new com.netflix.loadbalancer.RandomRule();
     }
